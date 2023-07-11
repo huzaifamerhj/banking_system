d92983e554ed7b96316e10f1e371c520059f44b6
royesh <muhammadhussian.royesh1@gmail.com>
2023-07-11 16:09:31 +0800

    Sakina just change the url of about us in view function

--- core/views.py
+++ core/views.py
@@ -1,4 +1,14 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
+from . import views
+
+def aboutus(request):
+    return render(request,'core/base.html')
+    
+
+
+
 
 
 class HomeView(TemplateView):
