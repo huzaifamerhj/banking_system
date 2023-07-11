2e8306a8f4716081bd1dd098a4414ec50f794035
royesh <muhammadhussian.royesh1@gmail.com>
2023-07-11 19:28:24 +0800

    transfer updated

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
 
 
 class HomeView(TemplateView):
     template_name = 'core/index.html'
+
+def aboutus(request):
+    return render(request, 'core/about_us.html')
